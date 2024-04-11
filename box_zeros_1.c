1511a2e88750d8fb30854326f3d6016b7c94dd7a
Loshan <126599956+loshan-malaka-kaluarachchi@users.noreply.github.com>
2024-04-11 13:40:01 +0530

    Update and rename prints a box of zeros mod_1.txt to box_zeros_1.c

--- box_zeros_1.c
+++ box_zeros_1.c
@@ -1,25 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
-                /** printing (row by column) box of zeros: mod_1 **/
 
+/** printing (row by column) box of zeros: mod_1 **/
 int main()
 {
-    int row,col;
-
+    int row, col;
     printf("Enter no of rows~> ");
     scanf("%d",&row);
     printf("Enter no of columns~> ");
     scanf("%d",&col);
 
     int i=0;
-
     while(i<row)
     {
-
         int j=0;
         while(j<col)
         {
-
             if(j%12==0)
             {
                 printf("0");
@@ -28,14 +24,10 @@ int main()
             {
                 printf("1");
             }
-
-
             j=j+1;
         }
         printf("\n");
         i=i+1;
-
     }
-
     return 0;
 }
